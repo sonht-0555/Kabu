91c033296b08af76def57572968bba883d2bcd59
Son Hoang Tuan <hoang.tuan.son@sun-asterisk.com>
2025-12-19 15:22:37 +0700

    --- Build Ver_2.07 ---

--- src/js/global.js
+++ src/js/global.js
@@ -1,4 +1,4 @@
-tag("body"), tag("page00"), tag("page01"), tag("page02"), tag("logo"), tag("page02"), tag("notification"), tag("display"), tag("list"), tag("list01"), tag("list02"), tag("name"), tag("ver"), tag("gamepad"), tag("title1"), tag("vertical"), tag("screen"), tag("invis"), tag("joypad"), tag("green0"), tag("white0"), tag("message0");
+tag("html"), tag("body"), tag("page00"), tag("page01"), tag("page02"), tag("logo"), tag("page02"), tag("notification"), tag("display"), tag("list"), tag("list01"), tag("list02"), tag("name"), tag("ver"), tag("gamepad"), tag("title1"), tag("vertical"), tag("screen"), tag("invis"), tag("joypad"), tag("green0"), tag("white0"), tag("message0");
 let gameName, gameType, gameWidth, gameHeight, integer, timerId, count = null, Module = null, canSync = true, recCount = 1, isReload = false, swipe;
 const canvas = document.getElementById('canvas');
 let [hours, minutes, seconds, count1] = [0, 0, 0, 0, 0];
@@ -72,6 +72,6 @@ async function gameView(romName) {
     screen.style.setProperty("--shader", svgGen(window.devicePixelRatio, integer, local(`shader0${local("shader")}`) || "0.0.0.1.0.0.1.0.0.1.0.0.1.0.0.0"));
 }
 document.addEventListener("DOMContentLoaded", function(){
-    setTimeout(() => {body.removeAttribute('hide')},100);
+    html.removeAttribute('hide')
     body.style.setProperty("--background", svgGen(1, window.devicePixelRatio, "0.0.1.0.1.0.1.0.0"));
 });
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let revision = 'Ver_2.06';
+let revision = 'Ver_2.07';
 var urlsToCache = [
     '/',
     './README.md',
